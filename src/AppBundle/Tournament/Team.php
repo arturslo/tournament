d86ad3a88df101ec10126a45c3304c8a0bb8b149
Artūrs Ločmelis <arturs.locmelis.89@gmail.com>
2018-10-31 10:15:58 +0200

    add team class

---
+++ src/AppBundle/Tournament/Team.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Tournament;
+
+
+class Team
+{
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Team constructor.
+     * @param string $name
+     */
+    public function __construct(string $name)
+    {
+        if (strlen($name) === 0) {
+            throw new \InvalidArgumentException('Name cannot be empty string!');
+        }
+
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+    }
+
+}
